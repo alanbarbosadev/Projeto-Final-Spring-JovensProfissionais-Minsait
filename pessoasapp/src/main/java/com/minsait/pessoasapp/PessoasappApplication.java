1df1da2b3503eb90814a7cf78341cdeacf74b599
Alan Barbosa <alanbarbosadev@gmail.com>
2023-10-30 08:11:18 -0300

    Criação Inicial

---
+++ pessoasapp/src/main/java/com/minsait/pessoasapp/PessoasappApplication.java
@@ -0,0 +1,13 @@
+package com.minsait.pessoasapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoasappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoasappApplication.class, args);
+	}
+
+}
---
+++ pessoasapp/src/test/java/com/minsait/pessoasapp/PessoasappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minsait.pessoasapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoasappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
